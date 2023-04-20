b263827f2abd537e1fbfd0d59007580ee3a7013b
“vstevenlab” <labsteven19@gmail.com>
2023-04-20 16:30:19 -0400

    Create vpc file

--- variables.tf
+++ variables.tf
@@ -13,3 +13,43 @@ variable "environment" {
  description = "environment"
  type        = string
 }
+#vpc variables
+
+variable "vpc_cidr" {
+ description = "vpc cidr block"
+ type        = string
+}
+
+#Public Subnet variables
+
+variable "public_subnet_az1_cidr" {
+ description = "public subnet cidr block"
+ type        = string
+}
+
+variable "public_subnet_az2_cidr" {
+ description = "public subnet az2 cidr block"
+ type        = string
+}
+
+
+variable "private_app_subnet_az1_cidr" {
+ description = "private app subnet az1 cidr block"
+ type        = string
+}
+
+
+variable "private_app_subnet_az2_cidr" {
+ description = "private app subnet az2 cidr block"
+ type        = string
+}
+
+variable "private_data_subnet_az1_cidr" {
+ description = "private data subnet az1 cidr block"
+ type        = string
+}
+
+variable "private_data_subnet_az2_cidr" {
+ description = "private data subnet az2 cidr block"
+ type        = string
+}
