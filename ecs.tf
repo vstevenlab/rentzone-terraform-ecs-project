ddec52e2a5c3665dcb33626b3d7ee2f83ab1cf42
vstevenlab <labsteven19@gmail.com>
2023-04-27 16:08:54 -0400

    ecs file

--- ecs.tf
+++ ecs.tf
@@ -48,7 +48,7 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
       environmentFiles = [
         {
           value = "arn:aws:s3:::${var.project_name}-${var.env_file_bucket_name}/${var.env_file_name}"
-          type  = s3
+          type  = "s3"
         }
       ]
 
